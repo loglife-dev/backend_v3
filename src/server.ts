cee68156398a60fa6166551c9f3f93774a1711e9
jhongomes <jhonatangomes859@gmail.com>
2021-08-30 07:56:24 -0300

    Adicionando swagger

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,15 @@ import "reflect-metadata";
 import "express-async-errors";
 import "./shared/infra/database";
 import "./shared/container"
-
+import swaggerUi from 'swagger-ui-express';
+import swaggerFile from "./swagger.json";
 import { router } from "./shared/infra/routes";
+
 import { AppError } from "./shared/errors/AppError";
 
 const app = express();
 app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile))
 app.use(router);
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
   if (err instanceof AppError) {
