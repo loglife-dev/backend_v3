5f6844f10ffd729ef40b287fc82e2342d87f0556
jhongomes <jhonatangomes859@gmail.com>
2021-09-01 16:23:36 -0300

    Update

--- src/modules/collector/repositories/ICollectorRepository.ts
+++
@@ -1,9 +0,0 @@
-import { IBaseRepository } from "../../../shared/infra/repositories/IBaseRepositories";
-import { Collector } from "../infra/typeorm/entities/Collector";
-
-
-export interface ICollectorRepository extends IBaseRepository<Collector>{
-
-    findById(id: string): Promise<Collector>;
-    findByCnpj()
-}
