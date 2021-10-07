93bf7be70f304f4c4e25515c95cbb9699ec231d5
jhongomes <jhonatangomes859@gmail.com>
2021-10-07 13:04:48 -0300

    update

--- src/modules/collectService/useCases/update/CollectServiceController.ts
+++ src/modules/collectService/useCases/update/CollectServiceController.ts
@@ -7,7 +7,7 @@ class UpdateCollectServiceController {
         const { id } = request.params
         const { service_id, address_id, driver_id, responsible_name, responsible_cpf, volume, sample, problem, box_photo, content_declaration, receipt_photo, departure_latitude, departure_longitude, departure_timestamp, unsuccess_latitude,
             unsuccess_longitude, unsuccess_timestamp, observation, hasUnsuccess } = request.body;
-
+        console.log(request.files)
         const updateCollectServiceUseCase = container.resolve(UpdateCollectServiceUseCase)
 
         const collectService = await updateCollectServiceUseCase.execute({
