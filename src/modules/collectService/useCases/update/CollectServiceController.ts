3a4dea191b1175d4b57d92328cf97ec449ac70e0
jhongomes <jhonatangomes859@gmail.com>
2021-10-13 13:59:19 -0300

    UPDATE

--- src/modules/collectService/useCases/update/CollectServiceController.ts
+++ src/modules/collectService/useCases/update/CollectServiceController.ts
@@ -9,27 +9,27 @@ class UpdateCollectServiceController {
         const { service_id, address_id, driver_id, provider_id, responsible_name, responsible_cpf, volume, sample, problem, departure_latitude, departure_longitude, departure_timestamp, unsuccess_latitude,
             unsuccess_longitude, unsuccess_timestamp, observation, hasUnsuccess } = request.body;
 
-        let box_photo = ''
-        let content_declaration = ''
-        let receipt_photo = ''
-
-        for (let file of files) {
-
-            if (file.fieldname === 'box_photo') {
-                box_photo = file.key
-            }
-            if (file.fieldname === 'content_declaration') {
-                content_declaration = file.key
-            }
-            if (file.fieldname === 'receipt_photo') {
-                receipt_photo = file.key
-            }
-        }
-
         const updateCollectServiceUseCase = container.resolve(UpdateCollectServiceUseCase)
         let collectService: any
 
         if (!hasUnsuccess) {
+            let box_photo = ''
+            let content_declaration = ''
+            let receipt_photo = ''
+
+            for (let file of files) {
+
+                if (file.fieldname === 'box_photo') {
+                    box_photo = file.key
+                }
+                if (file.fieldname === 'content_declaration') {
+                    content_declaration = file.key
+                }
+                if (file.fieldname === 'receipt_photo') {
+                    receipt_photo = file.key
+                }
+            }
+
             collectService = await updateCollectServiceUseCase.execute({
                 service_id: id,
                 address_id,
